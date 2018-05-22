e902301b55561cbc205682c5a8d027c9b089d214
sx <songxuan319@gmail.com>
2018-05-22 14:34:04 +0200

    Add ArrayList.removeIf() example to PredicateExample

--- src/predicate/PredicateExample.java
+++ src/predicate/PredicateExample.java
@@ -1,5 +1,7 @@
 package predicate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.function.Predicate;
 
 public class PredicateExample {
@@ -7,12 +9,31 @@ public class PredicateExample {
 	public static void main(String[] args) throws PredicateTestFailureException  {
 	
 		Predicate<String> p = i -> i.length() > 5;
-
+		
+		String[] al = {"dddd","dfasdfasf","sadfasf"};
+		
+		// convert the raw String array into an Arraylist.
+		ArrayList<String> ayString = new ArrayList<String>(Arrays.asList(al));
+		
+		/*	ArrayList.removeIf()
+		 * Removes all of the elements of this collection that satisfy the given predicate. 
+		Errors or runtime exceptions thrown during iteration or by the predicate are relayed to
+		the caller.*/
+		
+		ayString.removeIf(p);
+		
+		System.out.println(ayString);
+	
+		
+		System.out.println("====== String tests begin ======");
+		
+		//p.test(String s) outputs true or false.
 		if(p.test("123456")) {
 			PredicateExample.suvivedTheTest();
+
 		}
 		
-		if(p.test("12345")) {
+		if(p.test("123456")) {
 			PredicateExample.suvivedTheTest();
 		}else {
 			throw new PredicateTestFailureException();
