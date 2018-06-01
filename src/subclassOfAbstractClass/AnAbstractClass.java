57d076d8dd7c53222cdc9a24d8148e116dd92c54
sx <songxuan319@gmail.com>
2018-06-01 14:30:36 +0200

    An example of an abstract class extending another abstrct class

---
+++ src/subclassOfAbstractClass/AnAbstractClass.java
@@ -0,0 +1,7 @@
+package subclassOfAbstractClass;
+
+public abstract class AnAbstractClass {
+	// abstract class's methods can't be private. otherwise they can't be inherited.
+	protected abstract void show();
+
+}
---
+++ src/subclassOfAbstractClass/SubclassOfAnAbstractClass.java
@@ -0,0 +1,6 @@
+package subclassOfAbstractClass;
+
+public abstract class SubclassOfAnAbstractClass extends AnAbstractClass {
+	public abstract void abs();
+
+}
